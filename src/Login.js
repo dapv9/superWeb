e8a0ba213a4ff451cad4fcb9badd28522e011fa3
David Parra <david.parra@udea.edu.co>
2017-10-28 09:06:50 -0500

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -5,15 +5,15 @@ import ReactDOM from 'react-dom';
 export default class Login extends Component {
     render(){
       return(
-        <div>
         <center>
+        <div>
         <input type="text" placeholder="Usuario" onChange={this.login} onFocus={this.value=""}/>
-          <br>
+        <br>
         <input type="text" placeholder="Contraseña" onChange={this.login} onFocus={this.value=""}/>
-          <br>
+        <br>
         <input type="button" value="Ingresar " onClick={this.login}/>
-        </center>
         </div>
+        </center>
       )
   }
 }
