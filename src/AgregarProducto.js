dae103bfa6588193806a7bfabff6418a495b47a3
Sebas <sebastianlondo324@gmail.com>
2017-10-28 11:00:24 -0500

    Organizando agregar productos

--- src/AgregarProducto.js
+++ src/AgregarProducto.js
@@ -11,12 +11,16 @@ export default class agregarProducto extends Component {
       precio: " ",
       sku: " ",
       cantidad: " ",
-      existe: "",
+      existe: "Ingrese un nuevo producto",
+      auxiliar: false,
+      holi: " ",
+      agregado: false,
     }
     this.tomarNombre = this.tomarNombre.bind(this);
     this.tomarCantidad = this.tomarCantidad.bind(this);
     this.tomarSku = this.tomarSku.bind(this);
     this.tomarPrecio = this.tomarPrecio.bind(this);
+    this.guardarProducto = this.guardarProducto.bind(this);
   }
   tomarNombre(e) {
     this.setState({nombre: e.target.value})
@@ -34,23 +38,20 @@ export default class agregarProducto extends Component {
     this.setState({precio: e.target.value})
     console.log(this.state.precio);
   }
+
   guardarProducto() {
     this.setState({sku: " "});
     let boton = [];
     let termino = null;
     let auxiliar = null;
     ListaInventario.forEach((listaInventario) => {
-      if(listaInventario.sku !== auxiliar && listaInventario.sku == this.state.sku) {
-        this.setState({existe: "El producto ya existe"});
-      } else {
-        Inventario.forEach((inventario) => {
-          if(inventario.sku == null) {
-            inventario.nombre = this.setState.nombre;
-            inventario.cantidad = this.setState.cantidad;
-            inventario.sku = this.setState.sku;
-            inventario.precio = this.setState.precio;
-          }
-        });
+      if(listaInventario.sku == this.state.sku) {
+        this.setState({existe: "El producto ya existe", auxiliar: true});
+      }
+    });
+    ListaInventario.forEach((listaInventario) => {
+      if(listaInventario.sku == auxiliar && !this.state.agregado && this.state.auxiliar) {
+        this.setState({})
       }
     });
   }
@@ -64,6 +65,8 @@ export default class agregarProducto extends Component {
       <p><input type="text" placeholder="Precio" onChange={this.tomarPrecio} onFocus={this.value=""}/></p>
       <p><input type="text" placeholder="Referencia" onChange={this.tomarSku} onFocus={this.value=""}/></p>
       <p><input type="button" value="Guardar " onClick={this.guardarProducto}/></p>
+      <p>{this.state.existe}</p>
+      <p>{this.state.holi}</p>
       </center>
       </div>
     )
