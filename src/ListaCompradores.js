5e639849a6804353e773c71d92d7e12a6060c580
Sebas <sebastianlondo324@gmail.com>
2017-11-24 14:43:37 -0500

    Crear lista de cuentas

--- src/ListaCompradores.js
+++ src/ListaCompradores.js
@@ -1,4 +1,9 @@
 export default[
   {usuario:"comprador0", contraseña: "123", direccion: "Carrera 50#20-22"},
   {usuario:"comprador1", contraseña: "123", direccion: "Carrera 32#10-15"},
+  {usuario:"comprador2", contraseña: "123", direccion: "Carrera 43#14-83"},
+  {usuario:"comprador3", contraseña: "123", direccion: "Carrera 80#20-25"},
+  {usuario:"comprador4", contraseña: "123", direccion: "Carrera 56#93-10"},
+  {usuario:"comprador5", contraseña: "123", direccion: "Carrera 34#54-60"},
+  {usuario:"comprador6", contraseña: "123", direccion: "Carrera 45#45-40"},
 ]
--- src/ListaVendedores.js
+++ src/ListaVendedores.js
@@ -1,6 +1,6 @@
 export default[
   {usuario:"juansanchez", contraseña: "123"},
-  {usuario:"sebaslondo", contraseña: "123456543221"},
-  {usuario:"joanmarin", contraseña: "6543211234"},
-  {usuario:"davidparra", contraseña: "987654657"}
+  {usuario:"sebaslondo", contraseña: "123"},
+  {usuario:"joanmarin", contraseña: "123"},
+  {usuario:"davidparra", contraseña: "123"}
 ]
